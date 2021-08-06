85547056c3a4232da7711d9976250cba4d912384
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-06 16:01:51 +0300

    Add a list of names, tableView methods

--- Milesotne-Projects 1-3/ViewController.swift
+++ Milesotne-Projects 1-3/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var flags = [String]()
+   
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if(item.contains("png")){
+                flags.append(item)
+               
+            }
+        }
+        flags.sort()
+        print(flags.count)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = flags[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //move to detial view
+        
+    }
 
 }
 
