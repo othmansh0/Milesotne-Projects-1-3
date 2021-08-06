eb0db31b824eb09943f03fbafca386a0a98b9ede
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-06 14:49:14 +0300

    Initial Commit

---
+++ Milesotne-Projects 1-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milesotne-Projects 1-3
+//
+//  Created by othman shahrouri on 8/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
